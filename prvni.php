1dc6186649cfc259c6634f4812f4cc73442e1ded
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2020-02-25 08:22:59 +0100

    prvni.php

---
+++ prvni.php
@@ -0,0 +1,30 @@
+<?php
+$name = "Libor Pluháček";
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1><?php echo $name; ?></h1>
+    <ul>
+
+    <li> Datum narození: 22.5.88 </li>
+    <li> ZŠ:3ZŠ </li>
+    <li> E-mail:Pluhacek.libor@icloud.com </li>
+    <li>Tel.: 773161121 </li>
+    <li> Koníčky:sport </li>
+
+</ul>
+
+    
+</body>
+</html>
